dc56b63784e84d229b2f1981a641e75593500899
¨Esteban¨ <esti.03.oa@gmail.com>
2023-08-27 01:52:53 -0300

    clase 10

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ const apellido = "Gojo";
 
 console.log(nombre);
 console.log(apellido);
+
+console.log(nombre + " " + apellido);
+
+let num1 = "1";
+let num2 = "2";
+let suma = "1 + 2";
+
+console.log(1 + 2 + suma);
+
+let num3 = "suma + 4";
+
+console.log(suma + 4);
