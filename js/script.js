d82891fa127d7cde53caaaa1959fc2338fc7c66d
¨Esteban¨ <esti.03.oa@gmail.com>
2023-08-28 21:18:23 -0300

    clase 11 se pudo

--- js/script.js
+++ js/script.js
@@ -7,24 +7,111 @@ console.log(apellido);
 
 console.log(nombre + " " + apellido);
 
-let num1 = "1";
-let num2 = "2";
-let suma = "1 + 2";
+const num1 = 1;
+const num2 = 2;
+const suma = num1 + num2;
 
-console.log(1 + 2 + suma);
+console.log(suma);
 
-let num3 = "suma + 4";
+const num3 = suma + 4;
 
-console.log(suma + 4);
+console.log(num3);
 
 /* clase 11 */
-/* let topping 
-let precio
-let precioFinal
-let Helado = 10
+/* if */
+const toppingElegidoCliente = "Oreo";
 
-if (condition) {
-    
-} */
+const valorHelado = 100;
 
-/* Yo le mando la clase 11 pero la verdad no me sale, si me pueden ayudar porque no lo entendi del todo */
+const toppingKitKat = {
+  nombre: "kitkat",
+  precio: 15,
+};
+const toppingOreo = {
+  nombre: "oreo",
+  precio: 15,
+};
+const toppingKinder = {
+  nombre: "kinder",
+  precio: 15,
+};
+let precioFinal = 0;
+
+if (toppingElegidoCliente.toLowerCase() === toppingKitKat.nombre) {
+  /*  */
+  precioFinal = valorHelado + toppingKitKat.precio;
+  console.log(`El total a pagar es de : $ ${precioFinal}`);
+  /*  */
+} else if (toppingElegidoCliente.toLowerCase() === toppingOreo.nombre) {
+  /*  */
+  precioFinal = valorHelado + toppingOreo.precio;
+  console.log(`El total a pagar es de : $ ${precioFinal}`);
+  /*  */
+} else if (toppingElegidoCliente.toLowerCase() === toppingKinder.nombre) {
+  /*  */
+  precioFinal = valorHelado + toppingKinder.precio;
+  console.log(`El total a pagar es de : $ ${precioFinal}`);
+  /*  */
+} else {
+  console.log("El topping elegido no es válido.");
+}
+
+/* Switch */
+const comidaDelUsuario = "ensalada";
+const bebidas = ["vino tinto", "vino blanco", "agua"];
+switch (comidaDelUsuario) {
+  case "carne":
+    {
+      console.log(
+        `Con ${comidaDelUsuario} la mejor opción es un ${bebidas[0]}`
+      );
+    }
+
+    break;
+  case "pescado":
+    {
+      console.log(
+        `Con ${comidaDelUsuario} la mejor opción es un ${bebidas[1]}`
+      );
+    }
+
+    break;
+  case "pasta":
+    {
+      console.log(
+        `Con ${comidaDelUsuario} la mejor opción es un ${bebidas[2]}`
+      );
+    }
+
+    break;
+
+  default:
+    {
+      console.log("le ofrezco una mejor opción, hay carne, pescado o pasta");
+    }
+
+    break;
+}
+
+/* For */
+const arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+arrayNumeros.push(20);
+
+console.log(arrayNumeros);
+
+for (let i = 0; i < arrayNumeros.length; i++) {
+  const element = arrayNumeros[i];
+
+  console.log(element);
+}
+
+/* While */
+let numero = 0;
+const findelCiclo = 11;
+
+while (numero <= findelCiclo) {
+  console.log(numero);
+
+  numero++;
+}
